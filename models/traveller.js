353ffe4ecbedfd32282159232c2113538fcd5890
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-09 16:37:32 +0000

    Add Traveller getJourneyStartLocations function with passing test.

--- models/traveller.js
+++ models/traveller.js
@@ -4,7 +4,7 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
